ff5b6222852ce35228e4bc472fc1415849d60294
TeMecha Newton <tanewton0523@gmail.com>
2020-03-17 03:01:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,8 +64,11 @@ end
 def adding_matz
   programmer_hash =
   {
-    :
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
   }
+  
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
