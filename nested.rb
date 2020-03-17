c584e827510e2940a28c1c2db982903aea1e813a
TeMecha Newton <tanewton0523@gmail.com>
2020-03-17 03:10:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,6 +125,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:known_for] = 
+    programmer_hash[:dennis_ritchie][:known_for] = alans_new_info
 
 end
