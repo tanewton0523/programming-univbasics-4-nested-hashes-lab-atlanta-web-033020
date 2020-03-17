11d9f4126d8b31809c09cba9b19b35a8b426318e
TeMecha Newton <tanewton0523@gmail.com>
2020-03-17 03:01:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,6 +62,10 @@ def dennis_ritchies_language
 end
 
 def adding_matz
+  programmer_hash =
+  {
+    :
+  }
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
