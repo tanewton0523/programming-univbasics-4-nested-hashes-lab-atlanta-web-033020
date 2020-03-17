1e2af3da79331f99ee052d949a1ea457c1fd9da6
TeMecha Newton <tanewton0523@gmail.com>
2020-03-17 03:11:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,6 +125,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    alans_new_info = "GUI"
+    
     programmer_hash[:dennis_ritchie][:known_for] = alans_new_info
+    programmer_hash
 
 end
